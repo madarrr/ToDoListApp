4dcb1aad46eaa2053cde0c8f5cd30ab2e0d92051
madarrr <delaureaka@gmail.com>
2023-07-12 20:42:45 +0000

    creating frontend desk page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <title>ToDoList</title>
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+            <h2 class="card-title">ToDoList</h2>
+                            
+            <div class="form-outline">
+               <input type="search" id="form1" class="form-control" placeholder="Type query" aria-label="Search" />
+            </div>
+     
+
+        </div>
+   </div>
+    </div>
+</body>
+</html>
