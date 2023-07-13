936afbae58f5a2a0ca702051b22a7d5cf7a48288
madarrr <delaureaka@gmail.com>
2023-07-13 20:36:51 +0000

    saving progession

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,31 +11,45 @@
 </head>
 <body>
     <div class="container">
-                <div class="card">
-                    <div class="card-body">
-                            <h2 class="card-title">ToDoList</h2>
-                        <div class="row">
-                            <div class="form-outline col-md-6">
-                            <input type="search" id="form1" class="form-control" placeholder="Type query" aria-label="Search" />
-                            </div>
-                       </div>
-            <br>
-          <div class="card col-md-4" style="height:13rem;">
-            <div class="card-body">
-                <h5 class="card-title">Daily Task TRANS MFT</h5>
-                <h6 class="card-subtitle mb-2 text-muted">2 Hours</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <button type="submit" class="btn btn-danger"> <i class="fa fa-trash"></i></button>
-            </div>
-          </div>
-        
-            <!-- <div class="form-check form-switch">
-                <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
-                <label class="form-check-label" for="flexSwitchCheckDefault"> Daily task work on trans mft x6720</label>
-            </div> -->
-
-        </div>
-       </div>
-    </div>
+            <div class="card">
+                     <div class="card-body">
+                                 <h2 class="card-title">ToDoList</h2>
+                                     <div class="row">
+                                              <div class="form-outline col-md-6">
+                                                    <input type="search" id="form1" class="form-control" placeholder="Type query" aria-label="Search" />
+                                              </div>
+                                      </div>
+                                      <br>
+                                      <div class="row">
+                                      <div class="card col-md-3" style="height:13rem;" >
+                                            <div class="card-body">
+                                                <h5 class="card-title">Daily Task TRANS MFT</h5>
+                                                <h6 class="card-subtitle mb-2 text-muted">2 Hours</h6>
+                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                                <button type="submit" class="btn btn-danger"> <i class="fa fa-trash"></i></button>
+                                            </div>
+                                      </div> 
+                                      <div class="card col-md-3" style="height:13rem;" >
+                                            <div class="card-body">
+                                                <h5 class="card-title">Daily Task TRANS MFT</h5>
+                                                <h6 class="card-subtitle mb-2 text-muted">2 Hours</h6>
+                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                                <button type="submit" class="btn btn-danger"> <i class="fa fa-trash"></i></button>
+                                            </div>
+                                      </div> 
+                                      <div class="card col-md-3" style="height:13rem;" >
+                                            <div class="card-body">
+                                                <h5 class="card-title">Daily Task TRANS MFT</h5>
+                                                <h6 class="card-subtitle mb-2 text-muted">2 Hours</h6>
+                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                                <button type="submit" class="btn btn-danger"> <i class="fa fa-trash"></i></button>
+                                            </div>
+                                      </div>      
+                                      </div>      
+      
+                                </div>               
+             </div>       
+     </div>
 </body>
 </html>
+
