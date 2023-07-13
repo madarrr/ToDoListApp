9659964752df6ea5cd738468404ced1d5d2d73c1
madarrr <delaureaka@gmail.com>
2023-07-13 17:40:01 +0000

    editing home page adding card search bar and apply responsive rules

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,22 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/main.css">
     <title>ToDoList</title>
 </head>
 <body>
     <div class="container">
-        <div class="card">
+                <div class="card">
+                    <div class="card-body">
+                            <h2 class="card-title">ToDoList</h2>
+                        <div class="row">
+                            <div class="form-outline col-md-6">
+                            <input type="search" id="form1" class="form-control" placeholder="Type query" aria-label="Search" />
+                            </div>
+                       </div>
+            <br>
+          <div class="card col-md-4" style="height:13rem;">
             <div class="card-body">
-            <h2 class="card-title">ToDoList</h2>
-                            
-            <div class="form-outline">
-               <input type="search" id="form1" class="form-control" placeholder="Type query" aria-label="Search" />
+                <h5 class="card-title">Daily Task TRANS MFT</h5>
+                <h6 class="card-subtitle mb-2 text-muted">2 Hours</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button type="submit" class="btn btn-danger"> <i class="fa fa-trash"></i></button>
             </div>
-     
+          </div>
+        
+            <!-- <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+                <label class="form-check-label" for="flexSwitchCheckDefault"> Daily task work on trans mft x6720</label>
+            </div> -->
 
         </div>
-   </div>
+       </div>
     </div>
 </body>
 </html>
